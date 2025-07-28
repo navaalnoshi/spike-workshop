a5998336d872cf95ad725ff28122bcb372e290e9
navaalnoshi <navaalnoshi18@gmail.com>
2025-07-28 14:37:35 +0500

    Update power.c

--- project_navaalnoshi/task_4/power.c
+++ project_navaalnoshi/task_4/power.c
@@ -1,12 +1,25 @@
+#include <stdio.h>
 int power(int x, int n) {
     int result = 1;
+
     for (int i = 0; i < n; i++) {
         result *= x;
     }
+
     return result;
 }
 
 int main() {
-    int res = power(2, 10);
-    return res;
+    int base, exponent;
+
+    printf("Enter base (x): ");
+    scanf("%d", &base);
+
+    printf("Enter exponent (n): ");
+    scanf("%d", &exponent);
+
+    int result = power(base, exponent);
+    printf("%d^%d = %d\n", base, exponent, result);
+
+    return 0;
 }
